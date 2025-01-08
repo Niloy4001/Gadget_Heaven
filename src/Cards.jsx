f4cab96b2f977a4ea76fba6725b9d4751b3d63f8
Niloy4001 <huzaifasikderniloy58bd@gmail.com>
2025-01-08 17:32:32 +0600

    some code updated

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,55 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import { useLoaderData, useNavigate, useParams } from 'react-router-dom';
-import Card from './Card';
+import React, { useEffect, useState } from "react";
+import { useLoaderData, useNavigate, useParams } from "react-router-dom";
+import Card from "./Card";
 
 const Cards = () => {
-    const allGadgets = useLoaderData()
-    const { category } = useParams()
+  const allGadgets = useLoaderData();
+  const { category } = useParams();
 
+  const [allData, setAllData] = useState(allGadgets);
 
-    const [allData, setAllData] = useState(allGadgets);
+  useEffect(() => {
+    if (!category) {
+      setAllData([...allGadgets].slice(0, 7));
+    } else if (category === "All Product") {
+      setAllData(allGadgets);
+      // console.log(category);
+    } else {
+      const arr = [...allGadgets].filter((item) => item.category == category);
+      setAllData(arr);
+    }
+  }, [category, allGadgets]);
 
+  // console.log(allData.length);
 
-
-
-
-
-    useEffect(() => {
-        if (!category) {
-            setAllData([...allGadgets].slice(0, 7))
-
-        }
-
-        else if (category === 'All Product') {
-            setAllData(allGadgets)
-            // console.log(category);
-
-        } 
-        else {
-            const arr = [...allGadgets].filter((item) => item.category == category)
-            setAllData(arr)
-        }
-    }, [category, allGadgets])
-
-    // console.log(allData.length);
-
-    return (
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-
-            {
-                allData.length> 0 ?
-                allData.map((item) => <Card key={item.product_id} gadget={item}></Card>)
-                :
-                <div className='flex justify-center items-center'>
-                    <p className='font-bold text-3xl text-[#09080F] '>No data found</p>
-                </div>
-            }
-
-
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      {allData.length > 0 ? (
+        allData.map((item) => <Card key={item.product_id} gadget={item}></Card>)
+      ) : (
+        <div className="flex justify-center items-center">
+          <p className="font-bold text-3xl text-[#09080F] ">No data found</p>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Cards;
-
-
--- src/CategoriesSidebar.jsx
+++ src/CategoriesSidebar.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const CategoriesSidebar = ({ category }) => {
-    // console.log(category);
+  // console.log(category);
 
-    return (
-        <div className='w-[70%] mx-auto '>
-            <Link to={`category/${category.category}`}><button className='btn w-full rounded-3xl hover:bg-[#9538E2] bg-[#F7F7F7] hover:text-white'>{category.category}</button></Link>
-        </div>
-    );
+  return (
+    <div className="w-[70%] mx-auto ">
+      <Link to={`category/${category.category}`}>
+        <button className="btn w-full rounded-3xl hover:bg-[#9538E2] bg-[#F7F7F7] hover:text-white">
+          {category.category}
+        </button>
+      </Link>
+    </div>
+  );
 };
 
 export default CategoriesSidebar;
